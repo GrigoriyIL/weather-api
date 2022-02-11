8563ac9e6c051dcf69c70ce405f704f24d4e110c
Hryhorii <g.shubenko54@gmail.com>
2022-02-11 21:55:26 +0200

    add redonly field

--- weather/weatherapi/admin.py
+++ weather/weatherapi/admin.py
@@ -4,6 +4,7 @@ from weatherapi.models import *
 class WeatherAdmin(admin.ModelAdmin):
     list_display = ['location', 'date', 'weather', 'temperature', 'weather_feel','parse_date']
     list_display_links = ['location']
+    readonly_fields=['location', 'date', 'weather', 'temperature', 'weather_feel','parse_date']
 
 
 admin.site.register(Weather, WeatherAdmin)
